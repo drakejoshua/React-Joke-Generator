64312990cdffbef40a0094d4b35537818e8cfb57
Joshua Mabawonku <drakejoshua086@gmail.com>
2024-05-16 23:49:37 +0100

    feat: mobile design and animation integration
    - added mobile design, layout and styling using tailwind
    - added animations using tailwind to the loading and
      error display UI

--- src/App.jsx
+++ src/App.jsx
@@ -20,45 +20,45 @@ function App() {
 
   return (
     // app-markup-container
-    <div id="app" className="w-1/2 h-1/2 flex flex-col">
+    <div id="app" className="w-11/12 h-4/6 md:h-2/6 md:w-1/2 xl:h-1/2 flex flex-col">
 
       {/* quote ctn */}
       <div id="quote-ctn" className="w-full flex-grow">
 
         {/* loading pane - to display loading UI */}
         <div id="loading-pane" className="display-ui hidden">
-          <span className="material-symbols-outlined display-ui__display-icon">
+          <span className="material-symbols-outlined display-ui__display-icon animate-spin">
             autorenew
           </span>
 
-          <p className="display-ui__display-text">
+          <h2 className="display-ui__display-text">
             loading quote
-          </p>
+          </h2>
         </div>
         
         {/* error pane - to display error UI */}
-        <div id="error-pane" className="display-ui hidden">
-          <span className="material-symbols-outlined display-ui__display-icon">
+        <div id="error-pane" className="display-ui flex">
+          <span className="material-symbols-outlined display-ui__display-icon animate-pulse">
             sync_problem
           </span>
 
-          <p className="display-ui__display-text">
+          <h2 className="display-ui__display-text">
             error loading quote
-          </p>
+          </h2>
         </div>
 
         {/* loaded pane - to display quote, author and all other 
          data after loading from API*/}
-        <div id="loaded-pane">
+        <div id="loaded-pane" className="hidden">
 
           {/* language selection ctn */}
-          <div id="language-select-ctn">
+          <div id="language-select-ctn" className="flex gap-x-1">
             <label htmlFor="language-select" className="capitalize font-medium">
               language:
             </label>
 
             {/* language select */}
-            <select id="language-select" className="capitalize focus-within:outline-none focus:outline-none">
+            <select id="language-select" className="capitalize bg-white focus-within:outline-none focus:outline-none">
               {
                 languages.map( ( language, index ) => {
                   return (
@@ -74,24 +74,24 @@ function App() {
           </div>
           
           {/* quote ctn - contains quote, author, etc */}
-          <div id="quote-ctn" className="flex gap-x-4 items-center mt-16">
-            <span className="material-symbols-outlined text-9xl h-fit w-fit inline-block" style={ { transform: "scale(-1)" } }>
+          <div id="quote-ctn" className="flex gap-x-4 xl:items-center mt-16">
+            <span className="material-symbols-outlined text-3xl xl:text-9xl h-fit w-fit inline-block" style={ { transform: "scale(-1)" } }>
               format_quote
             </span>
 
             <div className="flex-grow-1">
-              <p id="quote" className="text-2xl italic font-medium mb-5">
+              <h1 id="quote" className="text-2xl italic font-medium mb-5">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Sit fugiat reprehenderit ad, facilis dolorum doloribus. 
                 Tempora ea esse sapiente architecto?
-              </p>
+              </h1>
 
               <p id="author" className="capitalize text-lg font-normal">
                 joshua mabawonku
               </p>
             </div>
 
-          </div>
+        </div>
 
         </div>
         
